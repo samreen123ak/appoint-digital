68c9f6eceb9ec78fe5d13d000973be4f16f20ac0
Samreen Akbar <104026248+samreen123ak@users.noreply.github.com>
2025-09-22 17:34:09 +0500

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,5 @@
 // import React from "react";
-// import Image from "next/image";
+import Image from "next/image";
 // import { FaInstagram, FaLinkedin, FaBehance } from "react-icons/fa";
 
 // const Footer: React.FC = () => {
